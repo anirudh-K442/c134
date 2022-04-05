6f70596b43844aa421a8cdc9459d37cb9d642f9e
anirudh-K442 <79716755+anirudh-K442@users.noreply.github.com>
2022-04-05 13:48:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function dinosaur() {
+    window.location = "dinosaur.html"
+}
+
+function teddy() {
+    window.location = "teddy.html"
+}
+
+function watch() {
+    window.location = "watch.html"
+}
+
+function gloves() {
+    window.location = "gloves.html"
+}
+
+function cube() {
+    window.location = "cube.html"
+}
